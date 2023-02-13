cfe984f54373431c293a073a9e3cba1742bc15e0
Lavanyagirl <124949915+Lavanyagirl@users.noreply.github.com>
2023-02-12 20:19:05 -0800

    Add files via upload

---
+++ TEMP.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+#define con 1.8
+void main()
+{
+float c,f;
+printf("enter the celsius values...");
+scanf("%f",&c);
+f=(con*c)+32;
+printf("\n fahren value of the given %f",f);
+//return(0);
+getch();
+}
+
